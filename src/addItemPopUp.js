1105ab107f7870a630ee778f416be430926a4b4a
Gary Arzumanyan <garyarzumanyan@gmail.com>
2020-12-23 00:16:18 -0800

    Add functionality to add task popup

--- src/addItemPopUp.js
+++ src/addItemPopUp.js
@@ -1,12 +1,36 @@
+import toDoItem from './toDoItem';
+
 const addItemPopUp = (() => {
     const myForm = document.getElementById('myForm');        
-        console.log(myForm);
 
     const showForm = () => myForm.style.display = "block";  
     const closeForm = () => myForm.style.display = "none";
 
-    const test = 50;
-    return{showForm, closeForm};
+
+    const handleFormInput = (e) => {
+        const myForm = e.target.form;
+    
+       const myItem = toDoItem(myForm.title.value, 
+                myForm.description.value, 
+                myForm.date.value, 
+                myForm.dropdown.value);
+
+       
+        closeForm();
+        //clearForm(myForm);
+        
+    }
+
+    const clearForm = (myForm) => {
+        
+        myForm.title.value = ""; 
+        myForm.description.value = ""; 
+        myForm.date.value = ""
+        myForm.dropdown.value = "1";
+    
+    }
+
+    return{showForm, closeForm, handleFormInput,};
 })();
  
  export default addItemPopUp;
--- src/eventListener.js
+++ src/eventListener.js
@@ -4,6 +4,12 @@ const eventListener = (() => {
     //+Task button event listener
     const addTaskButtonDiv = document.querySelector('.addTaskButton');        
     addTaskButtonDiv.addEventListener("click", addItemPopUp.showForm); //import addItemPopUp.js
+
+    const closePopUpDiv = document.querySelector('.closeButton');        
+    closePopUpDiv.addEventListener("click", addItemPopUp.closeForm); //import addItemPopUp.js
+
+    const submitTaskDiv = document.querySelector('.addButton');        
+    submitTaskDiv.addEventListener("click", addItemPopUp.handleFormInput); //import addItemPopUp.js
     
 })();
  
