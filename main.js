081da71cbe2f16b6028fbbea9353550027b39b5f
Gary Arzumanyan <garyarzumanyan@gmail.com>
2020-12-22 23:41:18 -0800

    Added +task button to show task adding popup

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";(()=>{const o=document.getElementById("myForm");console.log(o)})(),(()=>{const o=(o=>({title:o}))("My First Project");console.log(o)})()})();
+(()=>{"use strict";const o=(()=>{const o=document.getElementById("myForm");return console.log(o),{showForm:()=>o.style.display="block",closeForm:()=>o.style.display="none"}})();document.querySelector(".addTaskButton").addEventListener("click",o.showForm),(()=>{const o=(o=>({title:o}))("My First Project");console.log(o)})()})();
